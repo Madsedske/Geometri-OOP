839ff14bd0a21955254b108747c1e58dbf432291
zbcmber <zbcmber@ZBC-E-CH-SKP002>
2021-08-30 10:49:28 +0200

    Add project files.

---
+++ Geometri OOP/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv din a side ");
+            int side = Convert.ToInt32(Console.ReadLine());
+
+            Square sqauree = new Square(side);
+
+            Console.WriteLine("Omkreds " + sqauree.Perimeter(side) + " Areal " + sqauree.Area(side));
+            
+
+
+
+
+
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Geometri OOP/Square.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometri_OOP
+{
+    public class Square
+    {
+        private int side;
+
+        public int Side
+        {
+            get { return this.side; }
+            set { this.side = value; }
+        }
+
+        public Square(int Side)
+        {
+            side = Side;
+        }
+
+        public int Perimeter(int side)
+        {
+            return side * 4;
+        }
+
+        public int Area(int side)
+        {
+            return side * 2;
+        }
+    }
+}
